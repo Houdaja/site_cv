b37922156b5dafc4c8983ae371717be51096ffec
houdaja <houda.jaadar@lepoles.com>
2017-01-13 17:52:13 +0100

    modification du fichier competences

--- admin/competence.php
+++ admin/competence.php
@@ -1,59 +1,58 @@
-<?php require '../connexion/connexion.php'; 
+<?php require '../connexion/connexion.php';?> 
 
 
-
-
-if(isset($_POST['competence'])){ // On vérife si on a creer une nouvelle compétence
+<!-- if(isset($_POST['competence'])){ // On vérife si on a creer une nouvelle compétence
     if($_POST['competence']!= ''){//si comptence n'est pas vide
-    $competence = addslashes($_POST['competence']);
+    $competence = addslashes($_POST['competence']);*/
 
     //Pour inserer une nouvelle compétence
     /* $insert = $pdoCv->query("INSERT INTO t_competences(id_competence,competences,id_utilisateur) VALUES('','$competence','1') ")*/
-    $insert = $pdoCV->exec(" INSERT INTO t_competences VALUES (NULL,'$competence','1') ");
+/*    $insert = $pdoCV->exec(" INSERT INTO t_competences VALUES (NULL,'$competence','1') ");
         header("location:../admin/competence.php");
          exit();      
     }// on ferle le if
 }//on ferme le isset
-
+*/
 
 //pour supprimer une compétence
-if(isset($_GET['delete'])){
+/*if(isset($_GET['delete'])){
     $sql = 'DELETE FROM t_competences WHERE id_competence = "' . $_GET['delete'] . '"';
     $resultat = $pdoCV -> query($sql);
     header('location: competence.php');
 }
-
 ?>
+*/ -->
 
 <html lang="fr">
     <head>
     <meta charset="utf-8">
     <?php $sql = $pdoCV->query("SELECT * FROM t_utilisateur");
-
      $resultat = $sql->fetch();
-
- ?>
+    ?>
     <title><?php echo $resultat['prenom'].' ' .$resultat['nom'];?> - Compétences </title>
     <link rel="stylesheet" type="text/css" href="../css/style.css">
     </head>
-
         <body>
-        	<header>
+            <header>
             <!-- faire une entete -->
-      		</header>
-      		<h1 id="competence">Les compétences</h1>
+            </header>
+
             <div id="">
-                <h2>Connexion : déconnexion</h2>
+            <h1 id="competence">Les compétences</h1>
+               <!--  <h2>Connexion : déconnexion</h2> -->
                <?php //include("admin_menu.php"); ?><!-- FAUT CREER LA PAGE MENU -->
             </div>
-
             <div id="contenuPrincipal">
                 <div>
                     <form action="competence.php" method="post">
                         <table>
                             <tr>
-                                <td>Compétences</td> 
+                                <td id="t_competence">Compétences</td>
                                 <td><input type="text" name="competence" id="competence" size="50" required></td>                           
+                            </tr>
+                            <tr>
+                                <td>Niveau</td>
+                                <td><input type="text" name="niveau" id="niveau" size="50" required></td>                           
                             </tr>
                             <tr>
                                 <td colspan="2"><input type="submit" value"Insérer une compétences"></td>
