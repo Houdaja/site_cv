2dfa53cad71f8684af5adb96df8cca24d14218e4
houdaja <houda.jaadar@lepoles.com>
2017-01-18 17:04:17 +0100

    Ajout du front fonctionnel

---
+++ index02.php
@@ -0,0 +1,16 @@
+<?php require 'connexion/connexion.php';?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">   
+    <script src="https://use.fontawesome.com/30a190e011.js"></script>
+	<script src="../ckeditor/ckeditor.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>Developpeur & Integrateur Web</title>
+</head>
+<body>
+Bienvenue
+</body>
+</html>
